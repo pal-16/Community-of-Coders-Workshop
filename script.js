e92a54368cd1e297cd02a785b238c974abb8dbeb
pal_16 <mantrypalak@gmail.com>
2020-01-05 16:31:37 +0530

    html,css,js proejct for workshop

---
+++ script.js
@@ -0,0 +1,25 @@
+var x = document.getElementById("myAudio1");
+
+function playAudio1() {
+
+    x.play();
+}
+
+var y = document.getElementById("myAudio2");
+
+function playAudio2() {
+
+    y.play();
+}
+
+var z = document.getElementById("myAudio3");
+
+function playAudio3() {
+
+    z.play();
+}
+
+
+function display() {
+    alert('By the seniors,for the juniors!');
+}
